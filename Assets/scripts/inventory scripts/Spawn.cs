284fd8e9aec08850823bbad5221904f66e13edd2
lia harakis <146096583+itemlia@users.noreply.github.com>
2024-01-23 16:46:25 +0000

    update

--- Assets/scripts/inventory scripts/Spawn.cs
+++ Assets/scripts/inventory scripts/Spawn.cs
@@ -4,20 +4,21 @@ using UnityEngine;
 
 public class Spawn : MonoBehaviour
 {
-    public GameObject item;
+    private GameObject item;
     private Transform player;
     [SerializeField] private Vector3 scale;
 
     private void Start()
     {
         player = GameObject.FindWithTag("Player").transform; //gets position of player
+        item = GameObject.FindWithTag("item");
 
     }
 
     public void SpawnItems ()
     {
         Vector2 spawnPos = new Vector3(player.position.x + 3, player.position.y - 3, 0); //sets a new position variable of the players position with an offset
-        GameObject pickUp = Instantiate(item, spawnPos, Quaternion.identity); //quarternian srops object rotating weirdly when spawned back into gamae
-        pickUp.transform.localScale = scale; //sets scale of item when spawned into game
+        Instantiate(item, spawnPos, Quaternion.identity); //quarternian stops object rotating weirdly when spawned back into gamae
+        item.transform.localScale = scale; //sets scale of item when spawned into game
     }
 }
