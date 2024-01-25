fe5714c0a7422916e1b12e427c80b28402ed68b4
lia harakis <146096583+itemlia@users.noreply.github.com>
2024-01-25 14:51:41 +0000

    updateeeee

--- Assets/scripts/hoverDescription.cs
+++ Assets/scripts/hoverDescription.cs
@@ -6,23 +6,18 @@ using UnityEngine;
 public class hoverDescription : MonoBehaviour
 {
     public GameObject description;
-    private int hideAfterSeconds = 7;
+   
 
     private void OnMouseOver()
     {
-        Debug.Log("working");
+      
         description.SetActive(true);
-        //StartCoroutine(hideDescr());
+
     }
 
     private void OnMouseExit()
     {
         description.SetActive(false);
     }
-
-    //IEnumerator hideDescr()
-    //{
-    //    yield return new WaitForSeconds(hideAfterSeconds);
-    //    description.SetActive(false);
-    //}
 }
+    
