757d9a7fc7197c22e00c72eb347abdad299f583f
lia harakis <146096583+itemlia@users.noreply.github.com>
2024-01-29 14:46:19 +0000

    sclass

--- Assets/scripts/dialougeTimer.cs
+++ Assets/scripts/dialougeTimer.cs
@@ -1,16 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class dialougeTimer : MonoBehaviour
 {
 
    //[SerializeField] private int setActiveTime;
-    [SerializeField] private int destroyTime;
+    //[SerializeField] private int destroyTime;
     [SerializeField] private GameObject previousText;
+    private Button next;
 
-    private void Start()
+    private void OnMouseDown()
+    {
+        Debug.Log("mouse");
+        previousText.SetActive(false);
+    }
+
+
+
+    /*private void Start()
     {
         gameObject.SetActive(false);
         setActive();
@@ -32,5 +43,5 @@ public class dialougeTimer : MonoBehaviour
             Debug.Log("seen");
             gameObject.SetActive(true);
         }
-    }
+    }*/
 } 
