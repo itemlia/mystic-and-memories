07ee44560672c4723a979f38b0a90396f97621d4
lia harakis <146096583+itemlia@users.noreply.github.com>
2024-02-14 14:03:28 +0000

    yogurt shake

---
+++ Assets/scripts/lighting/globalTimer.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+public class globalTimer : MonoBehaviour
+{
+    
+    private Light2D lightComp;
+    public float repeatTime;
+    
+    
+
+    private void Start()
+    {
+        lightComp = GetComponent<Light2D>();
+
+      
+        for (int i = 0; i < 100; i++)
+        {
+            StartCoroutine(Timer());
+        }
+        
+    }
+
+   
+    IEnumerator Timer()
+    {
+        while (lightComp.intensity < 1)
+        {
+            yield return new WaitForSeconds(repeatTime);
+            lightComp.intensity = lightComp.intensity + (0.00008f);
+        }
+    }
+}
