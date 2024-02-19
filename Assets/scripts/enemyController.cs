5ffdc7c2926834d1ad0d8887a576fe3daaea6654
lia harakis <146096583+itemlia@users.noreply.github.com>
2024-02-19 13:24:22 +0000

    want so bad

--- Assets/scripts/enemyController.cs
+++ Assets/scripts/enemyController.cs
@@ -7,19 +7,25 @@ using UnityEngine.Rendering;
 public class enemyController : MonoBehaviour
 {
     private Transform player;
+
     public float speed;
     public float stoppingDist;
     public bool playerInSight;
     public int health = 100;
 
+    public GameObject healthBar;
+
+    public Vector3 scale;
+
     private void Start()
     {
         player = GameObject.FindWithTag("Player").GetComponent<Transform>();
-
+        healthBar = GameObject.Find("health bar");
     }
 
     private void Update()
     {
+     
         if (playerInSight && Vector2.Distance(transform.position, player.position) >= stoppingDist)
         {
             transform.position = Vector2.MoveTowards(transform.position, player.position, speed * Time.deltaTime);
@@ -48,7 +54,11 @@ public class enemyController : MonoBehaviour
     {
         if (playerInSight && health > 0 && Input.GetKeyDown(KeyCode.E))
         {
+            
+
             health = health - 10;
+            healthBar.transform.localScale = healthBar.transform.localScale - scale;
+            
         } else if (playerInSight && health <= 0 && Input.GetKeyDown(KeyCode.E))
         {
             Destroy(gameObject);
