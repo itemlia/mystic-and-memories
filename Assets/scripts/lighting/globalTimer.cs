f941f6543685d0934ac5ca2460626e9257557233
lia harakis <146096583+itemlia@users.noreply.github.com>
2024-02-22 10:08:44 +0000

    social oaath

--- Assets/scripts/lighting/globalTimer.cs
+++ Assets/scripts/lighting/globalTimer.cs
@@ -10,65 +10,24 @@ public class globalTimer : MonoBehaviour
     
     private Light2D lightComp;
     public float repeatTime;
+    
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
 
-
-    //private void OnEnable()
-    //{
-    //    SceneManager.sceneLoaded += OnSceneLoaded;
-    //}
-
-   // private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
-   //{
-   //     //when scene is loaded run function
-
-   //     lightComp = GetComponent<Light2D>();
-   //     GameObject[] lightDupe = GameObject.FindGameObjectsWithTag("global light");
-   //     string name = SceneManager.GetActiveScene().name;
-
-   //     if (name == "level1")
-   //     {
-   //         //if right scene is loaded sttart timer 
-
-   //         for (int i = 0; i < 100; i++)
-   //         {
-   //             if (lightDupe.Length > 1)
-   //             {
-   //                 //destroys extra global light if it duplicates when changing scene 
-
-   //                 Destroy(lightDupe[1]);
-                   
-   //                 lightComp = lightDupe[0].GetComponent<Light2D>();
-   //             }
-   //             else
-   //             {
-   //                 //starts coroutine timer and sets light to be active
-
-   //                 gameObject.SetActive(true);
-   //                 StartCoroutine(Timer());
-   //             }
-   //         }
-   //     } 
-   //     else 
-   //     {
-   //         //if in wrong scene disables light
-
-   //         gameObject.SetActive(false);
-   //     }
-
-   // }
-
-    private void Start()
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
     {
 
-        lightComp = GetComponent<Light2D>();
-        GameObject[] lightDupe = GameObject.FindGameObjectsWithTag("global light");
         string name = SceneManager.GetActiveScene().name;
+    
+        GameObject[] lightDupe = GameObject.FindGameObjectsWithTag("global light");
 
-        //keeps global light when changing scene so timer continues
-        DontDestroyOnLoad(lightComp);
 
         if (name == "level1")
         {
+            lightComp = GameObject.FindWithTag("global light").GetComponent<Light2D>();
+
             //if right scene is loaded sttart timer 
 
             for (int i = 0; i < 100; i++)
@@ -96,6 +55,16 @@ public class globalTimer : MonoBehaviour
 
             gameObject.SetActive(false);
         }
+    }
+
+    private void Start()
+    {
+
+        //keeps global light when changing scene so timer continues
+       
+        DontDestroyOnLoad(gameObject);
+
+       
     }
 
     //coroutine that delays when the intesity of the light decreases, aslong as intensity is more than 1
